1bfc83444b5f654dde16ba02d04f0944ddb9b236
Ryan Julyan <ryan@julyan.biz>
2024-12-02 05:16:50 +0200

    API Cookiecutter fixes

--- src/plugo/cli/cookiecutter-api-plugin/{{cookiecutter.plugin_slug}}/plugin.py
+++ src/plugo/cli/cookiecutter-api-plugin/{{cookiecutter.plugin_slug}}/plugin.py
@@ -1,7 +1,5 @@
-from flask import current_app
-
-from api.rest_api.routes import {{cookiecutter.plugin_slug}}_ns
+from .api.rest_api.routes import {{cookiecutter.plugin_slug}}_ns
 
 
 def init_plugin(app):
-    current_app.api.add_namespace({{cookiecutter.plugin_slug}}_ns)
+    app.api.add_namespace({{cookiecutter.plugin_slug}}_ns)
